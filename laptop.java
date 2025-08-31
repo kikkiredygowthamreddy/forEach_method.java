2ad212f006c0e018ca3b479b5a56a3adbf877c4d
kikkiredygowthamreddy <gowthamreddykikki6@gmail.com>
2025-08-31 10:08:41 +0530

    Add files via upload

---
+++ laptop.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.List;
+
+    public class laptop {  
+    public static void main(String[] args) {
+    List<Integer> num = Arrays.asList(5,6,9,1,11);
+   num.forEach(n -> System.out.println(n));
+   //   {
+   // Consumer<Integer> kia = (Integer n) -> System.out.println(n);
+   
+   
+    //  public void accept(Integer n)
+    //  {
+    //    System.out.println(num);
+    //  }
+    //    }; 
+// num.forEach(kia); 
+}
+    }
+  
